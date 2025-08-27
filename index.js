32cdbc11be6f6c4e85610ef35af3127e0a918f0a
Nishant Devam <nishantdevam@gmail.com>
2025-08-27 17:19:58 +0530

    converted two routes to a single for any date input to be confronted as a valid date or invalid date accordingly.

--- index.js
+++ index.js
@@ -6,41 +6,44 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+    res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/2015-12-25", function(req, res) {
-    var date= new Date("2015-12-25");
-    res.json({
-        unix: date.getTime(),
-        utc: date.toUTCString()
-    })
-});
+app.get('/api/:date?', function (req, res) {
+    var date;
+    if (!req.params.date) {
+        date = new Date();
+    } else if (!isNaN(req.params.date)) {
+        date = new Date(parseInt(req.params.date));
+    } else {
+        date = new Date(req.params.date);
+    }
 
-app.get("/api/1451001600000", function(req, res) {
-    var date= new Date(1451001600000);
-    res.json({
-        unix: date.getTime(),
-        utc: date.toUTCString()
-    })
+    if (date.toString() === 'Invalid Date') {
+        res.json({ error: 'Invalid Date' });
+    } else {
+        res.json({
+            unix: date.getTime(),
+            utc: date.toUTCString(),
+        });
+    }
 });
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
